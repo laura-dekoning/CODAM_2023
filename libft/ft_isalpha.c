95bcca1283094955e754a527a359c1690a6f5538
lauradekoning-codam <lde-koni@Student.codam.nl>
2023-10-03 07:50:17 +0200

    Added libft

---
+++ libft/ft_isalpha.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_isalpha.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lde-koni <lde-koni@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/03 07:38:37 by lde-koni      #+#    #+#                 */
+/*   Updated: 2023/10/03 07:43:50 by lde-koni      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+bool	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (true);
+	return (false);
+}
+
+int	main(void)
+{
+	int	c;
+
+	c = 'a';
+	if (ft_isalpha(c) == true)
+		printf("Is alphabetical\n");
+	if (ft_isalpha(c) == false)
+		printf("Is NOT alphabetical\n");
+	return (0);
+}
---
+++ libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libft.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lde-koni <lde-koni@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/03 07:39:09 by lde-koni      #+#    #+#                 */
+/*   Updated: 2023/10/03 07:40:49 by lde-koni      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+
+
+
+
+#endif
