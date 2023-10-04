4ef20726b8e9b17a9928d405b0bca1bff3ecec43
Laura De Koning <lade-kon@student.codam.nl>
2023-10-04 12:43:17 +0200

    tried something new in putnbr

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   ft_putnbr_fd.c                                     :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: lde-koni <lde-koni@student.codam.nl>         +#+                     */
+/*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/03 17:20:47 by lde-koni      #+#    #+#                 */
-/*   Updated: 2023/10/04 12:19:15 by lde-koni      ########   odam.nl         */
+/*   Updated: 2023/10/04 12:42:58 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@ void ft_putnbr_fd(int n, int fd)
 	{
 		ft_putchar_fd('-', fd);
 		n = n * -1;
-		ft_putnbr_fd(n, fd);
 	}
 	else if (n <= 9)
 		ft_putchar_fd((n + '0'), fd);
-		
-	else
+	if (n > 9)
 		ft_putnbr_fd((n / 10), fd);
 		ft_putnbr_fd((n % 10), fd);
-
 }
