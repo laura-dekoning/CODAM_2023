f16be32fb7fed0d182a95c4d1a433882a423f28a
lauradekoning-codam <lde-koni@Student.codam.nl>
2023-10-11 14:29:51 +0200

    added subject.pdf and memchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/03 15:36:26 by lde-koni      #+#    #+#                 */
-/*   Updated: 2023/10/04 12:27:53 by lade-kon      ########   odam.nl         */
+/*   Updated: 2023/10/11 14:23:49 by lde-koni      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@ char	*ft_strchr(const char *s, int c)
 	str = (char *)s;
 	while (*str != c)
 	{
-		str++;
 		if (!*s)
 			return (NULL);
+		str++;
 	}
 	return (str);
 }
