762bd0308c01b3fdcfe41621f11ea135d95050d8
Laura De Koning <lade-kon@student.codam.nl>
2023-11-30 14:38:16 +0100

    made ft_utils with numlen & strlen

---
+++ ft_printf/ft_utils.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_utils.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/30 13:58:36 by lade-kon      #+#    #+#                 */
+/*   Updated: 2023/11/30 14:36:37 by lade-kon      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ft_numlen(int n);
+{
+	int	len;
+
+	len = 0;
+	if (n < 0)
+	{
+		n = n * -1;
+		len++;
+	}
+	while (n > 9)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
